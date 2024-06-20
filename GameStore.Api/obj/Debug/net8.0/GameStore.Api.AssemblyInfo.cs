4f25e15115f2f29cfebacb93cbaa69cc92c4a568
ManSaint <143500481+ManSaint@users.noreply.github.com>
2024-06-20 12:09:43 +0200

    update

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c922a6f0f48974ff8387a898614f70794a856716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b283968545710d0719fe2d7d195b63a8b17db8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
